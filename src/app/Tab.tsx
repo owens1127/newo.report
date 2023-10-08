d331aa940d956d82351fc5b33fe091257b2f7b90
owensimpson <owensimpson1127@gmail.com>
2023-10-08 15:35:24 -0400

    add socials

--- src/app/Tab.tsx
+++ src/app/Tab.tsx
@@ -1,11 +1,43 @@
-export default function Tab({ title, url }: { title: string; url: string }) {
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Tab({
+  title,
+  url,
+  icon,
+  color,
+}: {
+  title: string;
+  url: string;
+  icon: string;
+  color: string;
+}) {
   return (
-    <a
+    <Link
       href={url}
-      className="flex w-full content-center justify-center border-b border-t border-neutral-800  bg-zinc-800/30 bg-gradient-to-b from-inherit pb-6 pt-8 backdrop-blur-2xl lg:static  lg:w-1/2 lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+      style={{ borderColor: color }}
+      className={`flex w-full transform content-center items-center justify-center gap-4 rounded-lg border bg-zinc-800/30 bg-gradient-to-b from-inherit pb-6 pt-8 backdrop-blur-2xl transition-transform hover:scale-105 lg:static  lg:w-1/2 lg:rounded-xl lg:border lg:bg-gray-200 lg:bg-zinc-800/30 lg:p-4`}
+      target="_blank"
     >
-      <div className="h-10 w-1 flex-1 border-r border-white">{""}</div>
-      <span className="flex-1">{title}</span>
-    </a>
+      <Image src={`/${icon}.png`} alt={`${icon} logo`} width={40} height={40} />
+      <div className="aspect-[1/5]">
+        <svg
+          width="8"
+          height="40"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 100"
+        >
+          <line
+            x1="10"
+            y1="10"
+            x2="10"
+            y2="90"
+            stroke={color}
+            strokeWidth="3"
+          />
+        </svg>
+      </div>
+      <span className="text-lg">{title}</span>
+    </Link>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,58 @@ import Tab from "./Tab";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center py-8 lg:p-24">
-      <header className="py-6">
+    <main className="flex min-h-screen flex-col items-center py-8">
+      <header className="py-6 lg:mb-8">
         <div className="container mx-auto text-center">
           <h1 className="text-4xl font-bold text-white">
             Welcome to <span className="text-blue-400">newo.report</span>
           </h1>
-          <p className="mt-2 text-xl text-white">Explore the Newo universe!</p>
         </div>
       </header>
-      {/* <div className="relative z-10 flex w-full max-w-5xl flex-col items-center justify-between font-mono text-sm lg:gap-3">
-        <Tab title="RaidHub" url="https://alpha.raidhub.app/newo" />
-        <Tab title="Twitch" url="https://twitch.tv/newox" />
-      </div> */}
+      <div className="relative z-10 flex w-full max-w-5xl flex-col items-center justify-between gap-2 font-mono text-sm lg:gap-3">
+        <Tab
+          title="RaidHub"
+          url="https://alpha.raidhub.app/newo"
+          icon="raidhub"
+          color="orange"
+        />
+        <Tab
+          title="Twitter"
+          url="https://twitter.com/kneewoah"
+          icon="twitter"
+          color="#26a7de"
+        />
+        <Tab
+          title="Twitch"
+          url="https://twitch.tv/newox"
+          icon="twitch"
+          color="#7302c9"
+        />
+        <Tab
+          title="YouTube"
+          url="https://youtube.com/@Newo1"
+          icon="youtube"
+          color="red"
+        />
+        <Tab
+          title="Discord"
+          url="https://discord.gg/aXuN3qwDRK"
+          icon="discord"
+          color="#5865F2"
+        />
+        <Tab
+          title="Dungeon Report"
+          url="https://dungeon.report/pc/4611686018488107374"
+          icon="raidreport"
+          color="#03a9f4"
+        />
+        <Tab
+          title="Raid Report"
+          url="https://raid.report/pc/4611686018488107374"
+          icon="raidreport"
+          color="#4db6ac"
+        />
+      </div>
     </main>
   );
 }
